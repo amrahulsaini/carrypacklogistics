229c5ae1b6ddeb308001a64a093bc2e5828ea959
amrahulsaini <ammrahulsaini@gmail.com>
2026-02-27 15:56:23 +0530

    style: increase body font weight to 500, headings to 700

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import NavigationProgress from "./components/NavigationProgress";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+  weight: ["400", "500", "600"],
 });
 
 const poppins = Poppins({
